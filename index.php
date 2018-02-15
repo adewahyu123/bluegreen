24a52edc415b920e382219453dd291c62c6051e9
Ade Wahyu <ADEW1@xl.co.id>
2018-02-15 15:54:47 +0700

    first app green app

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>This is Green App</title>
+<style>
+
+        *{
+            padding:0;
+            margin:0 auto;
+        }
+
+        #header{
+            height:80px;
+            background-color:green;
+            width:100%;
+        }
+
+        #content{
+            width:1500px;
+            background-color:red;
+        }
+
+</style>
+</head>
+<body>
+<div id="header"></div>
+<div id="content"><h1>TEST CloudFoundry Green</h1></div>
+</body>
+</html>
